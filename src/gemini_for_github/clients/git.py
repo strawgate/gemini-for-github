0adb2dd65d2e2d004d9083ae8dedfbccf528713c
strawgate <williamseaston@gmail.com>
2025-05-10 19:05:47 -0500

    Add return to starting branch method

--- src/gemini_for_github/clients/git.py
+++ src/gemini_for_github/clients/git.py
@@ -1,8 +1,8 @@
 from collections.abc import Callable
 
-from gemini_for_github.errors.git import GitBranchExistsError
 from git import Remote, RemoteReference, Repo
 
+from gemini_for_github.errors.git import GitBranchExistsError
 from gemini_for_github.shared.logging import BASE_LOGGER
 
 logger = BASE_LOGGER.getChild("git")
@@ -16,6 +16,8 @@ class GitClient:
     def __init__(self, repo_path: str = "."):
         self.repo: Repo = Repo(repo_path)
         self.origin: Remote = self.repo.remotes.origin
+        self.starting_branch: str = self.repo.active_branch.name
+
         # self.head: Head | None = None
 
     def get_tools(self) -> dict[str, Callable]:
@@ -24,6 +26,13 @@ class GitClient:
             "new_branch": self.new_branch,
             "push": self.push,
         }
+
+    def return_to_starting_branch(self):
+        """
+        Returns to the starting branch.
+        """
+        logger.info(f"Returning to starting branch: {self.starting_branch}")
+        self.repo.head.reference = self.repo.create_head(self.starting_branch)
 
     def new_branch(self, name: str):
         """
